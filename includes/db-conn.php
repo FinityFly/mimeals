d6bdfc91c7b8cc4fa8adf315b19334a75d764c9c
Finity <56236512+FinityFly@users.noreply.github.com>
2023-03-31 22:16:14 -0400

    Update db-conn.php

--- includes/db-conn.php
+++ includes/db-conn.php
@@ -1,7 +1,7 @@
 <?php
-$server_name = "localhost";
-$username = "root";
-$password = "";
+$server_name = "azureprivatedns.net";
+$username = "bgoictwmuo";
+$password = "C26L3E2V50WJ34I5$";
 $db_name = "mimeals_usr";
 $conn = mysqli_connect($server_name, $username, $password, $db_name);
 
