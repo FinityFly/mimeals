65591be6790775edfe488d876351d3569a28d821
Kiwi Disco <64095157+mhsJtsai@users.noreply.github.com>
2023-04-21 10:45:54 -0400

    thing

--- assets/js/meal-recipes.js
+++ assets/js/meal-recipes.js
@@ -1,2 +1,35 @@
-console.log('cum');
+
+// Popup functionality
+
+// get image
+const input = document.querySelector("#recipePhoto")
+const output = document.querySelector("output")
+let imagesArray = []
+input.addEventListener("change", function() {
+  const file = input.files
+  imagesArray.push(file[0])
+  let images = ""
+  imagesArray.forEach((image, index) => {
+    images += `<div class="image">
+                <img src="${URL.createObjectURL(image)}" alt="image">
+                <span onclick="deleteImage(${index})">&times;</span>
+              </div>`
+})
+output.innerHTML = images
+}
+
+
+
+
+// back button 
+// if the user types SOMETHING in the fields, then before exiting, ask if they are dsure htey want to discard their progress.
+
+// save button
+// const = document.querySelector("#save");
+// check if data is valid
+
+// we need:
+
+
+// save recipe data
 
