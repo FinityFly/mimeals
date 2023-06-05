13789c7270930488a51ca3fef235cdb9213e2e31
Jayden <jtsai3@ocdsb.ca>
2023-06-05 12:21:32 -0400

    comments

--- includes/signup-submit.php
+++ includes/signup-submit.php
@@ -3,6 +3,8 @@ session_start();
 include "db-conn.php";
 
 if (isset($_POST['email-input']) && isset($_POST['password-input'])) {
+    
+    // sanitize the data to prevent web attacks
     function validate($data) {
         $data = trim($data);
         $data = stripslashes($data);
@@ -15,10 +17,14 @@ if (isset($_POST['email-input']) && isset($_POST['password-input'])) {
     $upassword = validate($_POST['password-input']);
     $ucpassword = validate($_POST['cpassword-input']);
 
-
+    // A) Check if submitted email is a valid email address
     if (filter_var($uemail, FILTER_VALIDATE_EMAIL)) {
+
+        // check database if a user pre-exists with the submitted email
         $sql = "SELECT * FROM users WHERE email='$uemail'";
         $result = mysqli_query($conn, $sql);
+
+        // if a result appears, then this user already exists. Return an error message to the user.
         if (mysqli_num_rows($result)) {
             $row = mysqli_fetch_assoc($result);
             if ($row['email'] === $uemail) {
@@ -26,25 +32,37 @@ if (isset($_POST['email-input']) && isset($_POST['password-input'])) {
                 exit();
             }
         } else {
+
+            // C) check if two passwords match
             if ($upassword === $ucpassword) {
+
+                // hash the password for security and privacy purposes
                 $hashedPassword = hash('sha256', $upassword);
-                $sql = "INSERT INTO `users`(`id`, `email`, `password`, `name`) VALUES (NULL, '$uemail','$hashedPassword','$uname');";
-                mysqli_query($conn, $sql);
+                
+                //store the user's name, hashed password, and email in the database
+                $sql = "INSERT INTO `users`(`id`, `email`, `password`, `name`) 
+                                    VALUES (NULL, '$uemail','$hashedPassword','$uname');";
+                mysqli_query($conn, $sql);  
+
+                // uhm why are we doing this
                 $sql = "SELECT * FROM users WHERE email='$uemail'";
                 $result = mysqli_query($conn, $sql);
                 $row = mysqli_fetch_assoc($result);
-                // ADD THE NEW ACCOUNT LANDING PAGE HERE LATER
+
+                // create Session variables and redirect user to 'dashboard'
                 $_SESSION['email'] = $row['email'];
                 $_SESSION['name'] = $row['name'];
                 $_SESSION['id'] = $row['id'];
                 header("Location: ../dashboard.php");
                 exit();
             } else {
+                // C) if passwords do not match, return an error message to the user. 
                 header("Location: ../guest-signup.php?error=Passwords don't match");
                 exit();
             }
         }
     } else {
+        // A) If the submitted email is not valid address, return an error message to the user. 
         header("Location: ../guest-signup.php?error=Invalid email");
         exit();
     }
--- todo.md
+++ todo.md
@@ -13,3 +13,13 @@ add twitter, facebook and insta links
 
 ok the thing i was trying to tell you at costco was that we need to add a filter recipe search option, as in, I want a VEGAN meal or a DAIRY FREE meal... 
 - we prob wont get this done but if we do not bring it up, it will go by people's heads
+
+allow user to open custom recipes on the guest recipe page
+
+test the custom recipe stuff
+
+check the google doc for more stuff
+
+take care of the guest versions of the complciated pages, like guest-explroer.js instead of meal-explorer.js\
+
+fix calendar tab prev and next month bug
